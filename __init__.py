aac9ac57e0eded199800b8e705586521b4c1b6fd
bezdnov <dushkin.yan@gmail.com>
2026-03-31 01:48:42 +0700

    Realised some basic messages

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+import base64
+
 from flask import Flask, request, jsonify
 from uuid import uuid4
 from flask_socketio import SocketIO, send, emit, join_room, leave_room, rooms
@@ -10,11 +12,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = str(uuid4())
 
 SHARDING_FACTOR = 2
-REPLICATION_FACTOR = 2
+REPLICATION_FACTOR = 1
 
 
-socketio = SocketIO(app, logger=True, engineio_logger=True, async_mode='eventlet')
-
+socketio = SocketIO(app, logger=True, async_mode='eventlet')
 connected_users = {}
 
 # storage dict contains:
@@ -61,7 +62,6 @@ def handle_save_shard(shard_dict_string):
     for user in connected_users.keys():
         connected_users_list.append((user, connected_users[user]['client_kept'], connected_users[user]['client_saved']))
 
-
     # the top of the sorted list are the ones who kept the least and saved the most data into the network;
     # they must be our choice
     connected_users_list.sort(key=lambda x: x[1] - x[2] * REPLICATION_FACTOR)
@@ -69,58 +69,104 @@ def handle_save_shard(shard_dict_string):
     saved_amount = 0
     for user in connected_users_list:
         # we don't reuse the same user to store his data
-        if user == sender_sid:
+        if user[0] == sender_sid:
             continue
 
         # the has_shard is a "request" which is needed to check, if the client has some shard of file we're trying to save
         response = socketio.call('has_shard', {'FilePublicKey': shard_dict['FilePublicKey']}, to=user)
 
         # this already means that we can save the shard
-        if not response['has_shard']:
+        if not response:
             emit('save_shard', shard_dict, to=user)
             saved_amount += 1
             print('saving a shard to another user')
 
         # once we've saved to enough nodes, we can say that we've successfully saved a shard
         if saved_amount == REPLICATION_FACTOR:
-            emit('shard_saved', to=sender_sid)
+            emit('shard_saved', {'FilePublicKey': shard_dict['FilePublicKey']}, to=sender_sid)
             break
 
-    # "failed" case
     if saved_amount < REPLICATION_FACTOR:
-        emit('save_shard_ack', {'response': 'saving shard failed: not enough peers'}, to=sender_sid)
+        emit('save_shard_ack', {'response': 'saving shard failed: not enough peers', 'success': False}, to=sender_sid)
     else:
-        emit('save_shard_ack', {'response': 'saved successfully'}, to=sender_sid)
+        emit('save_shard_ack', {'response': 'saved successfully', 'success': True}, to=sender_sid)
 
 
-# receive
-# we'll believe, that the user is honest, and he keeps size of shards he sent to the network
+# This msg updates information on user
 @socketio.on('saved_size')
 def handle_saved_size(sizes_dict):
-    guid = sizes_dict['guid']
-    saved_in_network = sizes_dict['client_saved']
-    saved_in_client = sizes_dict['client_kept']
+    sid = request.sid
 
-    connected_users[guid]['client_kept'] = saved_in_client
-    connected_users[guid]['client_saved'] = saved_in_network
+    connected_users[sid]['client_kept'] = sizes_dict['client_kept']
+    connected_users[sid]['client_saved'] = sizes_dict['client_saved']
+
+    emit('saved_size_ack', {'response': 'saved sizes updates', 'success': True}, to=sid)
+
+
+# this mechanism is designed to proof that owner_sid is truly an owner of file file_public_key
+# it makes him decrypt a small 16-bytes message; if it succeeds,
+def check_file_ownership(owner_sid, file_public_key) -> bool:
+    # there are no blocking broadcast call, so...
+    for user in connected_users.keys():
+        if user != owner_sid:
+            response = socketio.call('has_shard', {'FilePublicKey': file_public_key}, to=user)
+            if response:
+                # response from client on this moment is base64 string
+                enc_proof_bytes_b64 = socketio.call(
+                    'get_proof_bytes', {'FilePublicKey': file_public_key}, to=user
+                )
+                dec_proof_bytes_b64 = socketio.call(
+                    'check_proof_bytes', {'FilePublicKey': file_public_key, 'ProofBytes': enc_proof_bytes_b64}, to=owner_sid
+                )
+                # verdict: is the one who is owner is truly an owner?
+                result = socketio.call(
+                    'compare_check_bytes', {'FilePublicKey': file_public_key, 'ProofBytesDecrypted': dec_proof_bytes_b64}, to=user
+                )
+                return result
+
+    return False
 
 
 # this "route" gets shards and returns it to request sender
 @socketio.on('get_file')
-def handle_get_file(get_file_data):
-    pass
+def handle_get_file(file_public_key):
+    sid = request.sid
+    is_owner = check_file_ownership(sid, file_public_key)
+
+    shards = []
+    if is_owner:
+        for i in range(SHARDING_FACTOR):
+            have_found = False
+            for user in connected_users.keys():
+                response = socketio.call('has_shard', {'FilePublicKey': file_public_key, 'ShardIndex': i}, to=user)
+                if response:
+                    emit('get_file_ack', {'success': True, 'response': f'Found shard index {i}'}, to=sid)
+                    shards.append(socketio.call('get_shard', file_public_key, to=user))
+                    have_found = True
+                    break
+            if not have_found:
+                emit('get_file_ack', {'success': False, 'response': f'Not found shard index {i}'}, to=sid)
+
+        emit('get_file', shards, to=sid)
 
 
 @socketio.on('delete_file')
-def handle_delete_file():
-    pass
+def handle_delete_file(file_public_key):
+    sid = request.sid
+    is_owner = check_file_ownership(sid, file_public_key)
+
+    if is_owner:
+        emit('delete_shard', file_public_key, broadcast=True)
 
 
+# has_shard message is already enough for checking file status.
+# response message can be just ignored
 @socketio.on('check_file')
-def handle_check_file(file_data):
-    print('here')
-    emit('check_file', file_data)
+def handle_check_file(file_public_key):
+    emit('has_shard', {'FilePublicKey': file_public_key}, broadcast=True)
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host="0.0.0.0", port=5000)
+
+
